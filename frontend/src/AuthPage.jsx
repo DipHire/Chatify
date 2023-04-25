939e419264c01f274dafa800584875e7314a5fc7
DipHire <diphire111@gmail.com>
2023-04-25 17:23:49 +0530

    updated localhost

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -4,7 +4,7 @@ const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
-axios.post('http://localhost:3001/authenticate', { username: value })
+axios.post('https://thisisachatapp.netlify.app/authenticate', { username: value })
   .then((r) => props.onAuth({...r.data, secret: value}))
   .catch((e) => {
     console.log("Auth Error", e.message); // error message
